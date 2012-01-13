989e38d6548486508d99ee9096d07ab509d1fb75
James Parham <jamey@theparhams.net>
2012-01-13 06:56:21 -0700

    Added methods for getting controller object by name and also by name and port number

--- src/util/jinput/JinputUtilities.java
+++ src/util/jinput/JinputUtilities.java
@@ -11,11 +11,16 @@ import net.java.games.input.Controller.Type;
 import net.java.games.input.ControllerEnvironment;
 
 /**
- *
+ * A utility class for the JInput library
  * @author Parham
  */
 public class JinputUtilities {
     
+    /**
+     * Get a list of all available controllers
+     * plugged in to the computer
+     * @return 
+     */
     public static List<Controller> availableGamepads() {
         List<Controller> controllerList = new ArrayList<Controller>();
         ControllerEnvironment env = ControllerEnvironment.getDefaultEnvironment();
@@ -26,6 +31,39 @@ public class JinputUtilities {
             }
         }
         return controllerList;
+    }
+    
+    /**
+     * Get a controller object by name.
+     * @param controllerName - the name of the controller
+     * @return the Controller object
+     */
+    public static Controller getControllerByName(String controllerName){
+        ControllerEnvironment env = ControllerEnvironment.getDefaultEnvironment();
+        Controller[] controllers = env.getControllers();
+        for (Controller c : controllers){
+            if (c.getName().equals(controllerName)){
+                return c;
+            }
+        }
+        return null;
+    }
+    
+    /**
+     * Get a controller object my name and port number
+     * @param controllerName - the controller name
+     * @param portNumber - the port number
+     * @return 
+     */
+    public static Controller getControllerByName(String controllerName, int portNumber){
+        ControllerEnvironment env = ControllerEnvironment.getDefaultEnvironment();
+        Controller[] controllers = env.getControllers();
+        for (Controller c : controllers){
+            if (c.getName().equals(controllerName) && c.getPortNumber() == portNumber){
+                return c;
+            }
+        }
+        return null;
     }
     
 }
