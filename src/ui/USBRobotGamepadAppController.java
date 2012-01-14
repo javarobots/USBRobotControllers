14993491922ae52a4946326baccb3a3095a51b78
James Parham <jamey@theparhams.net>
2012-01-14 00:22:05 -0700

    COmpleted start thread method

--- src/ui/USBRobotGamepadAppController.java
+++ src/ui/USBRobotGamepadAppController.java
@@ -4,12 +4,22 @@
  */
 package ui;
 
+import configuration.Configuration;
 import gamepad.common.ControllerModel;
+import gamepad.common.GamepadFunction;
 import gamepad.common.GamepadThread;
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import net.java.games.input.Component;
 import net.java.games.input.Controller;
+import org.jdom.Element;
+import util.JdomWrapper;
 import util.jinput.JinputUtilities;
+import util.xml.JdomDocumentReader;
 
 /**
  *
@@ -30,12 +40,36 @@ public class USBRobotGamepadAppController {
 
     void startGamepadThread(String controllerName, String modelClassAndFileName) {
         try {
+            //Split the model and filename into separate Strings
             String[] splitClassAndFile = modelClassAndFileName.split("-");
-            ControllerModel m = (ControllerModel) Class.forName(splitClassAndFile[0]).newInstance();
+            //Get the controller and the controller model
             Controller gameController = JinputUtilities.getControllerByName(controllerName);
-            
-            
-            System.out.println("debug");
+            ControllerModel controllerModel = (ControllerModel) Class.forName("gamepad.models." + splitClassAndFile[0]).newInstance();
+            controllerModel.setController(gameController);
+            //Map all component names to components
+            Map<String,Component> componentNameToComponentMap = new HashMap<String,Component>();
+            for (Component c : gameController.getComponents()){
+                componentNameToComponentMap.put(c.getName(), c);
+            }
+            //Determine components and corresponding function to load            
+            JdomDocumentReader reader = new JdomDocumentReader(new File(Configuration.getInstance().getApplicationDirectory(),splitClassAndFile[1]));
+            Element rootElement = reader.initAndGetRoot();
+            List<Element> rootChildren = JdomWrapper.getChildren(rootElement);
+            for (Element e : rootChildren){
+                if (e.getName().equals("control")){
+                    //Extract the component and function
+                    Element componentElement = e.getChild("component");
+                    Element functionElement = e.getChild("function");
+                    GamepadFunction gf = (GamepadFunction) Class.forName("gamepad.functions." + functionElement.getValue()).newInstance();
+                    gf.setComponent(componentNameToComponentMap.get(componentElement.getValue()));
+                    controllerModel.addFunction(gf);
+                }
+            }
+            //Set up the thread
+            GamepadThread gt = new GamepadThread(controllerModel);
+            mModel.setThread(gt);
+            Thread t = new Thread(gt);
+            t.start();
         } catch (InstantiationException ex) {
             Logger.getLogger(USBRobotGamepadAppController.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
@@ -43,6 +77,10 @@ public class USBRobotGamepadAppController {
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(USBRobotGamepadAppController.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+
+    void stopThread() {
+        mModel.getThread().stopThread(true);
     }
     
 }
