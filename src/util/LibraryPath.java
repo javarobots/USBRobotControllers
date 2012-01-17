e0a29fe8f0a1c2f0796592769366705d1f4f0621
unknown <jparham@592-r113-05.cec.loc>
2012-01-17 16:07:42 -0700

    Added LibraryPath to project for determining where to install rxtx and jinput support .dll files

---
+++ src/util/LibraryPath.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+/**
+ *
+ * @author jparham
+ */
+public class LibraryPath {
+    
+    public static void main(String[] args){
+        System.out.println(System.getProperty("java.library.path"));
+    }
+    
+}
