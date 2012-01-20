2bacf36434730c39934dde18c32bee01601986c6
James Parham <jamey@theparhams.net>
2012-01-20 13:40:02 -0700

    New method added for getting SerialPort

--- src/util/rxtx/RxTxUtilities.java
+++ src/util/rxtx/RxTxUtilities.java
@@ -4,17 +4,23 @@
  */
 package util.rxtx;
 
+import commonutilities.swing.ComponentPosition;
+import configuration.Configuration;
 import gnu.io.CommPortIdentifier;
+import gnu.io.NoSuchPortException;
+import gnu.io.PortInUseException;
+import gnu.io.SerialPort;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
+import ui.dialog.ErrorDialog;
 
 /**
  * A utility class for the RxTx library
  * @author jparham
  */
 public class RxTxUtilities {
-    
+
     public static List<String> getAvailablePorts(){
         List<String> portNames = new ArrayList<>();
         Enumeration identifiers = CommPortIdentifier.getPortIdentifiers();
@@ -26,5 +32,18 @@ public class RxTxUtilities {
         }
         return portNames;
     }
-    
+
+    public static SerialPort openPortByName(String name){
+        SerialPort port = null;
+        try {
+            CommPortIdentifier identifier = CommPortIdentifier.getPortIdentifier(name);
+            port = (SerialPort)identifier.open("USBRobotCOntroller", Configuration.OPEN_COMM_PAUSE);
+        } catch (PortInUseException | NoSuchPortException ex) {
+            ErrorDialog dialog = new ErrorDialog(null,true,ex.getMessage());
+            ComponentPosition.centerFrame(dialog);
+            dialog.setVisible(true);
+        }
+        return port;
+    }
+
 }
