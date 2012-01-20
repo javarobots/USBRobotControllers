8481278d01b28450caef95ebe20e4967e00b3a00
James Parham <jamey@theparhams.net>
2012-01-20 13:35:18 -0700

    Added sending data to serial port

--- src/gamepad/common/GamepadThread.java
+++ src/gamepad/common/GamepadThread.java
@@ -4,9 +4,14 @@
  */
 package gamepad.common;
 
+import gnu.io.SerialPort;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import ui.USBRobotGamepadAppModel;
+import ui.dialog.ErrorDialog;
+import util.rxtx.RxTxUtilities;
 
 /**
  * A Thread that runs continuously generating the
@@ -14,7 +19,7 @@ import ui.USBRobotGamepadAppModel;
  * @author Parham
  */
 public class GamepadThread implements Runnable {
-    
+
     private boolean mStopThread = false;
     private ControllerModel mModel;
     private USBRobotGamepadAppModel mApplicationModel;
@@ -22,22 +27,43 @@ public class GamepadThread implements Runnable {
     public GamepadThread(ControllerModel model){
         mModel = model;
     }
-    
+
     @Override
     @SuppressWarnings("SleepWhileInLoop")
     public void run() {
-        while (!mStopThread){
+        OutputStream out = null;
+        try {
+            //Open up the port to send from
+            SerialPort port = RxTxUtilities.openPortByName(mApplicationModel.getSelectedComPortName());
+            out = port.getOutputStream();
+            while (!mStopThread){
+                try {
+                    String cmd = mModel.generateCommand();
+                    byte[] cmdAsBytes = cmd.getBytes();
+                    out.write(cmdAsBytes);
+                    mApplicationModel.setCommandString(cmd);
+                    mApplicationModel.notifyObservers();
+                    Thread.sleep(250);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(GamepadThread.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+
+            //Close the port previously opened
+            port.close();
+            
+        } catch (IOException ex) {
+            ErrorDialog dialog = new ErrorDialog(mApplicationModel.getParentFrame(), true, ex.getMessage());
+            dialog.setVisible(true);
+        } finally {
             try {
-                String cmd = mModel.generateCommand();
-                mApplicationModel.setCommandString(cmd);
-                mApplicationModel.notifyObservers();
-                Thread.sleep(250);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(GamepadThread.class.getName()).log(Level.SEVERE, null, ex);
+                out.close();
+            } catch (IOException ex) {
+
             }
         }
     }
-    
+
     /**
      * Stop the thread from running
      * @param b - true to stop
